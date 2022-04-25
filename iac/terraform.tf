b65cc26a1be71db641815e9274fdd393172d3774
Belmiro Meneses Vieira Lima <33463784+belmiromeneses@users.noreply.github.com>
2022-04-25 17:40:42 -0300

    Add Sleep Linode Terraform

--- iac/terraform.tf
+++ iac/terraform.tf
@@ -44,7 +44,7 @@ resource "digitalocean_droplet" "cluster-manager" {
     inline = [
       "hostnamectl set-hostname cluster-manager",
       "apt -y update",
-      "sleep 2",
+      "sleep 5",
       "apt -y upgrade",
       "apt -y install curl wget htop unzip dnsutils",
       "export K3S_TOKEN=${var.digitalocean_token}",
